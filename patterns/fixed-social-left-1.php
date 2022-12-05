dd7755d2ffa84ed3027989947326cb7dd5989252
iidastudio <philip.w.shotaro@gmail.com>
2022-12-05 16:37:16 +0900

    initial commit

---
+++ patterns/fixed-social-left-1.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Title: Fixed area
+ * Slug: biblio/fixed/social/left/1
+ * Categories: Fixed
+ * Keywords: fixed social left
+ * Block Types: biblio/fixed
+ */
+?>
+<!-- wp:group {
+  "className":"p-biblio-fixed-social-left-1",
+  "style": {
+    "spacing": {
+      "margin": {
+        "top": "0"
+      }
+    }
+  }
+} -->
+<div class="wp-block-group p-biblio-fixed-social-left-1" style="margin-top:0;">
+  <!-- wp:social-links {
+    "iconColor":"biblio/text",
+    "iconColorValue":"#333333",
+    "className":"is-style-logos-only",
+    "layout":{"type":"flex","orientation":"vertical"}
+  } -->
+  <ul class="wp-block-social-links has-icon-color is-style-logos-only">
+    <!-- wp:social-link {"service":"twitter", "url":"https://twitter.com/"} /-->
+    <!-- wp:social-link {"service":"instagram", "url":"https://instagram.com/"} /-->
+    <!-- wp:social-link {"service":"youtube", "url":"https://youtube.com/"} /-->
+  </ul>
+  <!-- /wp:social-links -->
+</div>
+<!-- /wp:group -->
