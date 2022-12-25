f13648ba71f44aab9ff23a43cccef291d0c81575
iidastudio <philip.w.shotaro@gmail.com>
2022-12-25 19:17:15 +0900

    Changed how block per css is loaded

--- inc/Theme_Support.php
+++ inc/Theme_Support.php
@@ -16,17 +16,17 @@ class Theme_Support {
     add_action( 'wp_enqueue_scripts', [ $this, 'add_scripts_and_styles' ] );
     add_action( 'wp_enqueue_scripts', [ $this, 'set_script_translations' ] );
     add_filter( 'should_load_separate_core_block_assets', '__return_true' );
-    add_action( 'wp_enqueue_scripts', [ $this, 'biblio_enqueue_wp_core_block_styles'] );
-    add_action( 'admin_init', [ $this, 'biblio_enqueue_wp_core_block_styles'] ); 
+    add_filter( 'after_setup_theme', [ $this, 'biblio_enqueue_wp_block_styles'] );
     add_action( 'enqueue_block_editor_assets', [ $this, 'biblio_block_variation_enqueue'] );
     remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
     remove_action( 'wp_print_styles', 'print_emoji_styles', 10 );
-    // add_action( 'init', [ $this, 'remove_core_block_patterns' ], 9 );
+    add_action( 'init', [ $this, 'remove_core_block_patterns' ], 9 );
+    add_action('enqueue_block_editor_assets', [ $this, 'remove_core_block_patterns' ]);
   }
 
-  // public function remove_core_block_patterns() {
-  //   remove_theme_support( 'core-block-patterns' );
-  // }
+  public function remove_core_block_patterns() {
+    remove_theme_support( 'core-block-patterns' );
+  }
 
   // add_theme_support
   public function add_theme_supports() {
@@ -49,94 +49,108 @@ class Theme_Support {
     wp_set_script_translations( 'js-common', 'biblio', get_template_directory().'/languages' );
   }
 
+  // コアのブロックに付与するスタイル
+  private $styled_wp_blocks = array(
+    'archives',
+    'audio',
+    'avatar',
+    'buttons',
+    'button',
+    'calendar',
+    'categories',
+    'code',
+    'columns',
+    'comment-author-name',
+    'comment-content',
+    "comment-date",
+    "comment-edit-link",
+    'comment-reply-link',
+    'comments',
+    'comments-pagination',
+    'comments-title',
+    'comment-template',
+    'cover',
+    'embed',
+    'freeform',
+    'gallery',
+    'group',
+    'heading',
+    'image',
+    'latest-posts',
+    'media-text',
+    'navigation',
+    'nextpage',
+    'page-list',
+    'paragraph',
+    'post-author',
+    'post-author-name',
+    'post-comments-form',
+    'post-content',
+    'post-excerpt',
+    'post-date',
+    'post-featured-image',
+    'post-navigation-link',
+    'post-template',
+    'post-terms',
+    'post-title',
+    'pullquote',
+    'quote',
+    'query-pagination',
+    'query-pagination-next',
+    'query-pagination-numbers',
+    'query-pagination-previous',
+    'rss',
+    'search',
+    'separator',
+    'site-logo',
+    'site-tagline',
+    'site-title',
+    'social-links',
+    'spacer',
+    'table',
+    'template-part',
+    'verse',
+  );
+
+  private $styled_biblio_blocks = array();
+
   // 使用されたブロックのみcssを読み込む
-  public function biblio_enqueue_wp_core_block_styles() {
-    // コアのブロックに付与するスタイル
-    $styled_blocks = array(
-      'archives',
-      'audio',
-      'avatar',
-      'buttons',
-      'button',
-      'calendar',
-      'categories',
-      'code',
-      'columns',
-      'comment-author-name',
-      'comment-content',
-      "comment-date",
-      "comment-edit-link",
-      'comment-reply-link',
-      'comments',
-      'comments-pagination',
-      'comments-title',
-      'comment-template',
-      'cover',
-      'embed',
-      'freeform',
-      'gallery',
-      'group',
-      'heading',
-      'image',
-      'latest-posts',
-      'media-text',
-      'navigation',
-      'nextpage',
-      'page-list',
-      'paragraph',
-      'post-author',
-      'post-author-name',
-      'post-comments-form',
-      'post-content',
-      'post-excerpt',
-      'post-date',
-      'post-featured-image',
-      'post-navigation-link',
-      'post-template',
-      'post-terms',
-      'post-title',
-      'pullquote',
-      'quote',
-      'query-pagination',
-      'query-pagination-next',
-      'query-pagination-numbers',
-      'query-pagination-previous',
-      'rss',
-      'search',
-      'separator',
-      'site-logo',
-      'site-tagline',
-      'site-title',
-      'social-links',
-      'spacer',
-      'table',
-      'template-part',
-      'verse',
+  public function biblio_enqueue_wp_block_styles() {
+
+    $styled_blocks_arrays = array(
+      "wp" => $this -> styled_wp_blocks,
+      "biblio" => $this -> styled_biblio_blocks
     );
 
-    foreach ( $styled_blocks as $block_name ) {
-      $handle = (
-      function_exists( 
-        'wp_should_load_separate_core_block_assets' ) &&
-        wp_should_load_separate_core_block_assets()
-      ) ? 
-        "wp-block-$block_name" :
+    foreach($styled_blocks_arrays as $blocks_name => $styled_blocks) {
+      // wp-blocksのハンドル名をcoreにする(他のブロックは同一を前提)
+      $blocks_name === "wp"? $blocks_handle_name = "core" : $blocks_handle_name = $blocks_name;
+
+      foreach ( $styled_blocks as $block_name ) {
+        $handle = (
+        function_exists( 
+          'wp_should_load_separate_core_block_assets' ) &&
+          wp_should_load_separate_core_block_assets()
+        ) ?
+        "$blocks_name-block-$block_name":
         'wp-block-library';
 
-      $dir = "assets/css/wp-blocks/$block_name/";
-      $extension = ".css";
-      $editor_suffix = "-editor";
+        $dir = "assets/css/$blocks_name-blocks/$block_name/";
+        $style_path = $dir.$block_name.".css";
+        $editor_style_path = $dir.$block_name."-editor.css";
 
-      if( file_exists(get_theme_file_path( $dir.$block_name.$extension)) ) {
-        // get styles.
-        $styles = file_get_contents( get_theme_file_path( $dir.$block_name.$extension ) );
+        if( file_exists( get_theme_file_path( $style_path ) ) ) {
 
-        wp_add_inline_style( $handle, $styles ); // add front
-        add_editor_style( $dir.$block_name.$extension ); // add editor
-        
-      }
-      if ( file_exists( get_theme_file_path( $dir.$block_name.$editor_suffix.$extension ) ) ) {
-        add_editor_style( $dir.$block_name.$editor_suffix.$extension ); // add editor
+          $block_args = array(
+            'handle' => $blocks_handle_name."/".$block_name,
+            'src'    => get_theme_file_uri( $style_path ),
+            'path'   => get_theme_file_path( $style_path )
+          );
+          wp_enqueue_block_style( $blocks_handle_name."/".$block_name, $block_args );
+        }
+        if ( file_exists( get_theme_file_path( $editor_style_path ) ) ) {
+          add_editor_style( $editor_style_path ); // add editor
+        }
       }
     }
   }
