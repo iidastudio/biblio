68ee55411510458b7df00bceeded2fd38f23216e
iidastudio <philip.w.shotaro@gmail.com>
2023-02-13 02:31:31 +0900

    fix

--- inc/Theme_Update.php
+++ inc/Theme_Update.php
@@ -6,6 +6,7 @@
  * @since 1.0.0
  */
 namespace biblio\inc;
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -17,7 +18,7 @@ class Theme_Update {
 
   public function update_check() {
     require_once get_template_directory(). '/lib/plugin-update-checker/plugin-update-checker.php';
-    \Puc_v4_Factory::buildUpdateChecker(
+    PucFactory::buildUpdateChecker(
       'https://qim.chu.jp/release/biblio-info.json',
       get_template_directory(). '/functions.php',
       'biblio'
