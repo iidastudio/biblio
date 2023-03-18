a04307a971732720944cb96c60450c501cb0140e
iidastudio <philip.w.shotaro@gmail.com>
2023-03-18 16:47:03 +0900

    fix

--- inc/Custom_Post_Types.php
+++ inc/Custom_Post_Types.php
@@ -16,34 +16,89 @@ class Custom_Post_Types {
 
   public function create_post_type() {
     $showWritingFlg = get_option( 'biblio_admin_show_writing_flg' );
-    if( ! $showWritingFlg && ! $showWritingFlg == true ) {
-      return;
+    if( $showWritingFlg && $showWritingFlg == true ) {
+      register_post_type('writing', [ // 投稿タイプ名の定義
+        'labels' => [
+          'name' => '執筆', // 管理画面上で表示する投稿タイプ名
+          'singular_name' => '執筆', // カスタム投稿の識別名
+          'add_new' => '新しく執筆する',
+          'all_items' => '執筆物一覧',
+        ],
+        'public' => true, // 投稿タイプをpublicにするか
+        'has_archive' => true, // アーカイブ機能ON/OFF
+        'menu_position' => 5, // 管理画面上での配置場所(投稿の下に配置)
+        'show_in_rest' => true, // wordpress5.x系から出てきた新エディタ「Gutenberg」を有効にする
+        'supports' => array('title', 'editor', 'thumbnail', 'author', 'excerpt', 'comments', 'revisions', "post-formats"), // カスタム投稿で使用する項目を設定（タイトル、エディター、アイキャッチ）
+      ]);
+      /* タクソノミー(カテゴリー分け)の定義 */
+      register_taxonomy(
+        'writing-category', /* タクソノミーの名前 */
+        'writing', /* 使用するカスタム投稿タイプ名 */
+        array(
+          'hierarchical' => true, /* trueだと親子関係が使用可能。falseで使用不可 */
+          'update_count_callback' => '_update_post_term_count',
+          'label' => '執筆物カテゴリー',
+          'singular_label' => '執筆物カテゴリー',
+          'public' => true,
+          'show_ui' => true,// 管理画面表示ON/OFF
+        )
+      );
     }
-    register_post_type('writing', [ // 投稿タイプ名の定義
-      'labels' => [
-        'name' => '執筆', // 管理画面上で表示する投稿タイプ名
-        'singular_name' => '執筆', // カスタム投稿の識別名
-        'add_new' => '新しく執筆する',
-        'all_items' => '執筆物一覧',
-      ],
-      'public' => true, // 投稿タイプをpublicにするか
-      'has_archive' => true, // アーカイブ機能ON/OFF
-      'menu_position' => 5, // 管理画面上での配置場所(投稿の下に配置)
-      'show_in_rest' => true, // wordpress5.x系から出てきた新エディタ「Gutenberg」を有効にする
-      'supports' => array('title', 'editor', 'thumbnail', 'author', 'excerpt', 'comments', 'revisions', "post-formats"), // カスタム投稿で使用する項目を設定（タイトル、エディター、アイキャッチ）
-    ]);
-    /* タクソノミー(カテゴリー分け)の定義 */
-    register_taxonomy(
-      'writing-category', /* タクソノミーの名前 */
-      'writing', /* 使用するカスタム投稿タイプ名 */
-      array(
-        'hierarchical' => true, /* trueだと親子関係が使用可能。falseで使用不可 */
-        'update_count_callback' => '_update_post_term_count',
-        'label' => '執筆物カテゴリー',
-        'singular_label' => '執筆物カテゴリー',
+    $showBookFlg = get_option( 'biblio_admin_show_book_flg' );
+    if( $showBookFlg && $showBookFlg == true ) {
+      register_post_type('book', [
+        'labels' => [
+          'name' => '本',
+          'singular_name' => '本',
+          'add_new' => '新しく本をつくる',
+          'all_items' => '本一覧',
+        ],
         'public' => true,
-        'show_ui' => true,// 管理画面表示ON/OFF
-      )
-    );
+        'has_archive' => true,
+        'menu_position' => 5,
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor', 'thumbnail', 'author', 'excerpt', 'comments', 'revisions', "post-formats"),
+      ]);
+      register_taxonomy(
+        'book-category',
+        'book',
+        array(
+          'hierarchical' => true,
+          'update_count_callback' => '_update_post_term_count',
+          'label' => '本のカテゴリー',
+          'singular_label' => '本のカテゴリー',
+          'public' => true,
+          'show_ui' => true,
+        )
+      );
+    }
+    $showInfoFlg = get_option( 'biblio_admin_show_info_flg' );
+    if( $showInfoFlg && $showInfoFlg == true ) {
+      register_post_type('info', [
+        'labels' => [
+          'name' => '本',
+          'singular_name' => '本',
+          'add_new' => '新しく本をつくる',
+          'all_items' => '本一覧',
+        ],
+        'public' => true,
+        'has_archive' => true,
+        'menu_position' => 5,
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor', 'thumbnail', 'author', 'excerpt', 'comments', 'revisions', "post-formats"),
+      ]);
+      register_taxonomy(
+        'info-category',
+        'info',
+        array(
+          'hierarchical' => true,
+          'update_count_callback' => '_update_post_term_count',
+          'label' => '本のカテゴリー',
+          'singular_label' => '本のカテゴリー',
+          'public' => true,
+          'show_ui' => true,
+        )
+      );
+    }
   }
 }
