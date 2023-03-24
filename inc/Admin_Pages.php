5463bdaaa73c38669f61096ea650c502a1242356
iidastudio <philip.w.shotaro@gmail.com>
2023-03-24 09:38:06 +0900

    fix load css to match component colors

--- inc/Admin_Pages.php
+++ inc/Admin_Pages.php
@@ -15,6 +15,7 @@ class Admin_Pages {
   public function __construct() {
     if( is_admin() ) {
       add_action('admin_menu', [ $this, 'add_custom_pages' ]);
+      add_action( 'admin_enqueue_scripts', [ $this, 'biblio_admin_styles' ] );
     }
     // load page scripts.
     $biblio_settings_page = new Biblio_Settings_Page;
@@ -34,5 +35,14 @@ class Admin_Pages {
       },
       'dashicons-admin-settings'
     );
+  }
+
+  // 管理画面の配色と合わせるために読み込み
+  public function biblio_admin_styles() {
+    $screen = get_current_screen();
+    if( $screen->id === 'toplevel_page_biblio-settings' ) {
+      wp_register_style( 'edit-post-style', includes_url( '/css/dist/edit-post/style.min.css' ), array(), get_bloginfo( 'version' ) );
+      wp_enqueue_style( 'edit-post-style' );
+    }
   }
 }
