228c50b4148b4c856ce33d4fcc40ba1284296137
iidastudio <philip.w.shotaro@gmail.com>
2023-03-31 02:01:51 +0900

    fix

--- inc/extension/code_block_syntax/Code_Block_Syntax.php
+++ inc/extension/code_block_syntax/Code_Block_Syntax.php
@@ -14,6 +14,7 @@ class Code_Block_Syntax {
     if ( !is_admin() ) {
       wp_enqueue_style('style-prism', get_template_directory_uri().'/inc/extension/code_block_syntax/lib/prism.css', array(), filemtime( get_template_directory().'inc/extension/code_block_syntax/lib/prism.css' ));
       wp_enqueue_script('js-prism', get_template_directory_uri().'/inc/extension/code_block_syntax/lib/prism.js', array('wp-i18n'), filemtime( get_template_directory().'inc/extension/code_block_syntax/lib/prism.js' ) );
+      wp_script_add_data('js-prism', 'defer', true);
     }
   }
 
--- inc/extension/code_block_syntax/src/index.js
+++ inc/extension/code_block_syntax/src/index.js
@@ -1,9 +1,5 @@
-import { registerBlockType } from '@wordpress/blocks';
 import './style.scss';
-
 import Edit from './edit';
-import save from './save';
 
 const enableBlocks = 'core/code';
 Edit(enableBlocks);
-// save(enableBlocks);
