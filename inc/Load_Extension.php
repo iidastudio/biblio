82c2384c6e1718c3354e407dea0fcd5c9bd77904
iidastudio <philip.w.shotaro@gmail.com>
2023-04-04 01:31:39 +0900

    fix

--- inc/Load_Extension.php
+++ inc/Load_Extension.php
@@ -7,18 +7,11 @@
  */
 namespace biblio\inc;
 use biblio\inc\extension\code_block_syntax\Code_Block_Syntax;
-use biblio\lib\Helper;
 
 class Load_Extension {
   
   public function __construct() {
-    add_action( 'wp', array( $this, 'load_extension' ) );
-  }
-
-  public function load_extension() {
-    if( Helper::is_block_used_on_page( 'core/code' ) ) {
-      new Code_Block_Syntax;
-    }
+    new Code_Block_Syntax;
   }
 
 }
--- inc/extension/code_block_syntax/Code_Block_Syntax.php
+++ inc/extension/code_block_syntax/Code_Block_Syntax.php
@@ -6,19 +6,27 @@
  * @since 1.0.0
  */
 namespace biblio\inc\extension\code_block_syntax;
+use biblio\lib\Helper;
 
 class Code_Block_Syntax {
-  // JavaScriptファイルの読み込み
+
   public function __construct() {
     add_action( 'init', [$this, 'code_block_styntax_scripts'] );
     if ( !is_admin() ) {
-      wp_enqueue_style('style-prism', get_template_directory_uri().'/inc/extension/code_block_syntax/lib/prism.css', array(), filemtime( get_template_directory().'inc/extension/code_block_syntax/lib/prism.css' ));
-      wp_enqueue_script('js-prism', get_template_directory_uri().'/inc/extension/code_block_syntax/lib/prism.js', array('wp-i18n'), filemtime( get_template_directory().'inc/extension/code_block_syntax/lib/prism.js' ) );
-      wp_script_add_data('js-prism', 'defer', true);
+      add_action( 'wp_enqueue_scripts', [$this, 'load_prism_js'] );
     }
   }
 
   public function code_block_styntax_scripts() {
     register_block_type( __DIR__ . '/build' );
   }
+
+  public function load_prism_js() {
+    if( Helper::has_block_all('core/code') ) {
+      wp_enqueue_style('style-prism', get_template_directory_uri().'/inc/extension/code_block_syntax/lib/prism.css', array(), filemtime( get_template_directory().'inc/extension/code_block_syntax/lib/prism.css' ));
+      wp_enqueue_script('js-prism', get_template_directory_uri().'/inc/extension/code_block_syntax/lib/prism.js', array('wp-i18n'), filemtime( get_template_directory().'inc/extension/code_block_syntax/lib/prism.js' ) );
+      wp_script_add_data('js-prism', 'defer', true);
+    }
+  }
+
 }
