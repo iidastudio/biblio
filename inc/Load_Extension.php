76d4d11fb07bcc4df80c49c20e811ed04c438a44
iidastudio <philip.w.shotaro@gmail.com>
2023-04-08 19:01:19 +0900

    fix

--- inc/Load_Extension.php
+++ inc/Load_Extension.php
@@ -7,11 +7,14 @@
  */
 namespace biblio\inc;
 use biblio\inc\extension\code_block_syntax_highlight\Code_Block_Syntax_Highlight;
-
+use biblio\inc\extension\editor_support_hover_border\Editor_Support_Hover_Border;
 class Load_Extension {
   
   public function __construct() {
     new Code_Block_Syntax_Highlight;
+    if( is_admin() ) {
+      new Editor_Support_Hover_Border;
+    }
   }
 
 }
--- inc/Theme_Update.php
+++ inc/Theme_Update.php
@@ -12,6 +12,7 @@ defined( 'ABSPATH' ) || exit;
 class Theme_Update {
 
   private $old_transient_name;
+  private $now_transient_name;
 
   public function __construct() {
     if( is_admin() ) {
@@ -23,11 +24,12 @@ class Theme_Update {
 
   public function update_check( $transient ) {
 
-    $stylesheet = get_template();
-    $theme = wp_get_theme();
-    $version = $theme->get( 'Version' );
+    $theme_name = get_template();
+    $theme_data = wp_get_theme();
+    $version = $theme_data->get( 'Version' );
+    $this->now_transient_name = 'biblio-theme-update'.$version;
 
-    if( false == $remote = get_transient( 'biblio-theme-update'.$version ) ) {
+    if( false == $remote = get_transient( $this->now_transient_name ) ) {
 
       $remote = wp_remote_get(
         'https://qim.chu.jp/release/biblio-info.php',
@@ -55,11 +57,11 @@ class Theme_Update {
         return $transient; // who knows, maybe JSON is not valid
       }
 
-      set_transient( 'biblio-theme-update'.$version, $remote, HOUR_IN_SECONDS );
+      set_transient( $this->now_transient_name, $remote, HOUR_IN_SECONDS );
     }
 
-    $data = array(
-      'theme' => $stylesheet,
+    $remote_data = array(
+      'theme' => $theme_name,
       'url' => $remote->details_url,
       'requires' => $remote->requires,
       'requires_php' => $remote->requires_php,
@@ -67,6 +69,7 @@ class Theme_Update {
       'package' => $remote->download_url,
     );
 
+    $newTransient = new \stdClass();
     // check all the versions now
     if(
       $remote
@@ -74,17 +77,19 @@ class Theme_Update {
       && version_compare( $remote->requires, get_bloginfo( 'version' ), '<' )
       && version_compare( $remote->requires_php, PHP_VERSION, '<' )
     ) {
-      $this->old_transient_name = 'biblio-theme-update'.$version;
-      $transient->response[ $stylesheet ] = $data;
+      $this->old_transient_name = $this->now_transient_name;
+      $newTransient->response[ $theme_name ] = $remote_data;
     } else {
-      $transient->no_update[ $stylesheet ] = $data;
+      $newTransient->no_update[ $theme_name ] = $remote_data;
     }
 
-    return $transient;
+    return $newTransient;
   }
 
   // アップデート完了時に古いversionのtrasientを削除
   public function delete_old_transient() {
-    delete_transient( $this->old_transient_name );
+    if( $this->old_transient_name !== $this->now_transient_name ) {
+      delete_transient( $this->old_transient_name );
+    }
   }
 }
