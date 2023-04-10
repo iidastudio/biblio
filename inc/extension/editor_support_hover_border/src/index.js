dde5252354ff0d315770ccf0dc78ca3eaf06b6bd
iidastudio <philip.w.shotaro@gmail.com>
2023-04-11 01:15:51 +0900

    fix

--- inc/extension/editor_support_hover_border/src/index.js
+++ inc/extension/editor_support_hover_border/src/index.js
@@ -34,7 +34,21 @@ const HoverBorder = () => {
   if (isActive && !existingStyle) {
     const styleElement = document.createElement('style');
     styleElement.dataset.biblioHoverBorderData = 'enable';
-    styleElement.innerHTML = '.block-editor-block-list__block:hover { box-shadow: 0 0 0 1px var(--wp-admin-theme-color); }';
+    styleElement.innerHTML = `
+    .block-editor-block-list__layout .block-editor-block-list__block:hover:after {
+      border-radius: 1px;
+      bottom: 1px;
+      box-shadow: 0 0 0 var(--wp-admin-border-width-focus) var(--wp-admin-theme-color);
+      content: "";
+      left: 1px;
+      outline: 2px solid transparent;
+      pointer-events: none;
+      position: absolute;
+      right: 1px;
+      top: 1px;
+      z-index: 1;
+    }
+    `;
     document.head.appendChild(styleElement);
   } else if( !isActive && existingStyle ) {
     if (existingStyle) {
