f558efe681c960cb6226c582fba5c2e79c30f0f6
iidastudio <philip.w.shotaro@gmail.com>
2023-04-12 19:24:48 +0900

    fix

--- inc/extension/editor_support_hover_border/Editor_Support_Hover_Border.php
+++ inc/extension/editor_support_hover_border/Editor_Support_Hover_Border.php
@@ -12,11 +12,22 @@ use biblio\lib\Helper;
 class Editor_Support_Hover_Border {
 
   public function __construct() {
-      add_action( 'enqueue_block_editor_assets', [$this,  'editor_support_hover_border_scripts'] );
+    add_action( 'enqueue_block_editor_assets', [$this,  'editor_support_hover_border_scripts'] );
+    add_action( 'enqueue_block_editor_assets', [$this, 'php_to_js'] );
   }
 
-  function editor_support_hover_border_scripts() {
+  public function editor_support_hover_border_scripts() {
     register_block_type( __DIR__ . '/build' );
+  }
+
+  public function php_to_js () {
+    wp_localize_script( 
+      'biblio-extension-editor-support-hover-border-editor-script', 
+      'phpToJs', 
+      array(
+        'isGutenbergActive' => is_plugin_active( 'gutenberg/gutenberg.php' )
+      )
+    );
   }
   
 }
--- inc/extension/editor_support_hover_border/src/index.js
+++ inc/extension/editor_support_hover_border/src/index.js
@@ -1,12 +1,11 @@
-import { registerPlugin } from '@wordpress/plugins';
+import { registerPlugin, withPluginContext } from '@wordpress/plugins';
 import { PluginMoreMenuItem } from '@wordpress/edit-post';
-import { withPluginContext } from '@wordpress/plugins';
 import { PreferenceToggleMenuItem, store as preferencesStore } from '@wordpress/preferences';
 import { useSelect } from '@wordpress/data';
+import { useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 const HoverBorderToggleMenuItem = () => {
-
   return (
     <PreferenceToggleMenuItem
       label="Hover border mode"
@@ -17,43 +16,82 @@ const HoverBorderToggleMenuItem = () => {
   );
 };
 
+
+
 const HoverBorderPluginMoreMenuItem = withPluginContext( ( context ) => ( {
   as: HoverBorderToggleMenuItem,
 } ) )( PluginMoreMenuItem );
 
+
+const createHoverBorderStyle = () => {
+  const styleElement = document.createElement('style');
+  styleElement.dataset.biblioHoverBorder = 'enable';
+  const addStyle = `
+.block-editor-block-list__layout .block-editor-block-list__block:hover::after {
+border-radius: 1px;
+bottom: 1px;
+box-shadow: 0 0 0 var(--wp-admin-border-width-focus) var(--wp-admin-theme-color);
+content: "";
+left: 1px;
+outline: 2px solid transparent;
+pointer-events: none;
+position: absolute;
+right: 1px;
+top: 1px;
+z-index: 1;
+}
+  `;
+
+  styleElement.insertAdjacentHTML('beforeend', addStyle);
+  return styleElement;
+}
+
+const getIframeBody = () => {
+  const iframe = document.querySelector('iframe[name="editor-canvas"]');
+  if (iframe) {
+    const iframeDoc = iframe.contentWindow.document;
+    const iframeBody = iframeDoc.querySelector('head');
+    return iframeBody;
+  }
+};
+
 const HoverBorder = () => {
 
+  // この機能がオンかオフか
   const isActive = useSelect(
 		( select ) => !! select( preferencesStore ).get( "core/preferences", "biblio-hover-border-mode" ),
 		[ "biblio-hover-border-mode" ]
 	);
 
-  const dataName = "[data-biblio-hover-border-data]";
-  const existingStyle = document.querySelector(dataName);
+  const dataName = "[data-biblio-hover-border]";
 
-  if (isActive && !existingStyle) {
-    const styleElement = document.createElement('style');
-    styleElement.dataset.biblioHoverBorderData = 'enable';
-    styleElement.innerHTML = `
-    .block-editor-block-list__layout .block-editor-block-list__block:hover:after {
-      border-radius: 1px;
-      bottom: 1px;
-      box-shadow: 0 0 0 var(--wp-admin-border-width-focus) var(--wp-admin-theme-color);
-      content: "";
-      left: 1px;
-      outline: 2px solid transparent;
-      pointer-events: none;
-      position: absolute;
-      right: 1px;
-      top: 1px;
-      z-index: 1;
-    }
-    `;
-    document.head.appendChild(styleElement);
-  } else if( !isActive && existingStyle ) {
-    if (existingStyle) {
+  // Gutenbergプラグインが有効化されているか判定（Editor_Support_Hover_Boderからwp_localize_scriptを使用して取得）
+  const isGutenbergActive = phpToJs.isGutenbergActive;
+
+  // Gutenberg有効時（iframe使用のため分岐）
+  if (isGutenbergActive) {
+
+    const iframeBody = getIframeBody();
+    const existingStyle = iframeBody?.querySelector(dataName);
+
+    if (isActive && !existingStyle ) {
+      const hoverBorderStyle = createHoverBorderStyle();
+      iframeBody.appendChild(hoverBorderStyle);
+    } else if ( !isActive && existingStyle ) {
       existingStyle.remove();
     }
+
+  // Gutenberg無効時
+  } else {
+
+    const existingStyle = document.querySelector(dataName);
+    if ( isActive  && !existingStyle ) {
+      const hoverBorderStyle = createHoverBorderStyle();
+      document.head.appendChild(hoverBorderStyle);
+    } else if ( !isActive && existingStyle ) {
+      existingStyle.remove();
+    }
+
   }
 
   return (
