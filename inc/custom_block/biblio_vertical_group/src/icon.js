583cb19b60858fad781ad823e397224aeefabe66
iidastudio <philip.w.shotaro@gmail.com>
2023-07-17 15:23:46 +0900

    first commit

---
+++ inc/custom_block/biblio_vertical_group/src/icon.js
@@ -0,0 +1,16 @@
+export default (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    focusable="false"
+  >
+    <path d="M0,0H1.5V6.5H0Z" transform="translate(16.5 11.5)"/>
+    <path d="M0,0H1.5V6.5H0Z" transform="translate(13 11.5)"/>
+    <path d="M0,0H1.5V12H0Z" transform="translate(9.5 6)"/>
+    <path d="M0,0H1.5V6H0Z" transform="translate(6 6)"/>
+    <path d="M0,0H5V4H0Z" transform="translate(13 6)"/>
+    <path d="M2,1.5a.5.5,0,0,0-.5.5V16a.5.5,0,0,0,.5.5H16a.5.5,0,0,0,.5-.5V2a.5.5,0,0,0-.5-.5H2M2,0H16a2,2,0,0,1,2,2V16a2,2,0,0,1-2,2H2a2,2,0,0,1-2-2V2A2,2,0,0,1,2,0Z" transform="translate(3 3)"/>
+  </svg>
+)
