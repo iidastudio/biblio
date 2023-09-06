df37d5fad8abd9072f3a020ef6f6f35393ded66f
iidastudio <philip.w.shotaro@gmail.com>
2023-09-06 11:26:43 +0900

    first commit

---
+++ patterns/hero-1.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Title: Hero Image and Slogan
+ * Slug: biblio/hero/1
+ * Categories: hero
+ * Keywords: biblio hero image slogan
+ * Block Types: biblio/hero
+ */
+?>
+
+<!-- wp:group {"align":"full","style":{"dimensions":{"minHeight":"65vh"},"spacing":{"blockGap":"var:preset|spacing|80"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull" style="min-height:65vh"><!-- wp:columns {"isStackedOnMobile":false,"style":{"spacing":{"blockGap":{"left":"0"},"padding":{"right":"0","left":"0"}}}} -->
+<div class="wp-block-columns is-not-stacked-on-mobile" style="padding-right:0;padding-left:0"><!-- wp:column {"width":"20vw"} -->
+<div class="wp-block-column" style="flex-basis:20vw"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":""} -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:image {"align":"wide","id":70,"style":{"border":{"radius":{"topLeft":"80px","bottomLeft":"80px"}}}} -->
+<figure class="wp-block-image alignwide has-custom-border"><img src="http://localhost:8888/wp-content/uploads/2023/02/brent-olson-nbjvTN4nKfE-unsplash_resize.jpg" alt="" class="wp-image-70" style="border-top-left-radius:80px;border-bottom-left-radius:80px"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:group {"style":{"dimensions":{"minHeight":"25vh"},"spacing":{"padding":{"right":"var:preset|spacing|70","left":"var:preset|spacing|70"},"blockGap":"var:preset|spacing|30"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="min-height:25vh;padding-right:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"lg"} -->
+<p class="has-lg-font-size" style="font-style:normal;font-weight:700">サイト・サークル・会社等の名前</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"fontSize":"xxxl"} -->
+<h2 class="wp-block-heading has-xxxl-font-size">誰かに届くキャッチコピー</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
