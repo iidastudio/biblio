aad87bc41eb31a1a9b4d5306762ab13aa9403b6a
iidastudio <philip.w.shotaro@gmail.com>
2023-10-27 18:16:03 +0900

    load  prism.js and css as veiw.js and css

--- src/extensions/code_block_syntax_highlight/Code_Block_Syntax_Highlight.php
+++ src/extensions/code_block_syntax_highlight/Code_Block_Syntax_Highlight.php
@@ -5,28 +5,23 @@
  * @package BIBLIO
  * @since 1.0.0
  */
-namespace biblio\inc\extension\code_block_syntax_highlight;
+namespace biblio\dist\extensions\code_block_syntax_highlight;
 use biblio\lib\Helper;
 
 class Code_Block_Syntax_Highlight {
 
   public function __construct() {
-    add_action( 'init', [$this, 'code_block_styntax_scripts'] );
     if ( !is_admin() ) {
       add_action( 'wp_enqueue_scripts', [$this, 'load_prism_js'] );
     }
   }
 
-  public function code_block_styntax_scripts() {
-    register_block_type( __DIR__ . '/build' );
-  }
-
-  private $dir_name = '/inc/extension/code_block_syntax_highlight/lib/';
+  private $dir_name = '/dist/extensions/code_block_syntax_highlight/';
 
   public function load_prism_js() {
     if( Helper::has_block_all('core/code') ) {
-      wp_enqueue_style('style-prism', get_template_directory_uri().$this->dir_name.'prism.css', array(), filemtime( get_template_directory().$this->dir_name.'prism.css' ));
-      wp_enqueue_script('js-prism', get_template_directory_uri().$this->dir_name.'prism.js', array('wp-i18n'), filemtime( get_template_directory().$this->dir_name.'prism.js' ), array('strategy' => 'defer') );
+      wp_enqueue_style('style-prism', get_template_directory_uri().$this->dir_name.'view.css', array(), filemtime( get_template_directory().$this->dir_name.'view.css' ));
+      wp_enqueue_script('js-prism', get_template_directory_uri().$this->dir_name.'view.js', array('wp-i18n'), filemtime( get_template_directory().$this->dir_name.'view.js' ), array('strategy' => 'defer') );
     }
   }
 
---
+++ src/extensions/code_block_syntax_highlight/view.js
@@ -0,0 +1,2 @@
+import './lib/prism';
+import './lib/prism.css';
